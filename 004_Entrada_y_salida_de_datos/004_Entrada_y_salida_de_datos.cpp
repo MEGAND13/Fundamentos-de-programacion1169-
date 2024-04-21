27e7a71d0567aeaf99b62148859be0c1b100bdce
MEGAND13 <156978608+MEGAND13@users.noreply.github.com>
2024-04-21 02:22:15 -0600

    Add files via upload

--- 004_Entrada_y_salida_de_datos/004_Entrada_y_salida_de_datos.cpp
+++ 004_Entrada_y_salida_de_datos/004_Entrada_y_salida_de_datos.cpp
@@ -8,38 +8,51 @@ int main()
 {
     bool requiere_cita;
     string cita;
-    string Nombre; 
+    string Nombre;
     char sexo;
     int edad;
     int peso;
     float estatura;
-    
-    cout << 
-        "Hola, requiere asistencia medica? \n si la respuesta es si seleccione 1 \n si la respuesta es no seleccione 0 ";
+    float IMC;
+
+    cout << "Hola, requiere asistencia medica? \n si la respuesta es si seleccione 1 \n si la respuesta es no seleccione 0 ";
     cin >> requiere_cita;
-    if (requiere_cita) { 
+    if (requiere_cita) {
         cita = "si";
         cout << "cual es tu nombre";
-        cin >> Nombre; 
-        cout << " Un placer atenderte" << Nombre;
-        cout << " Selecciona tu sexo M/F";
+        cin >> Nombre;
+        cout << " Un placer atenderte " << Nombre;
+        cout << " Selecciona tu sexo M/F ";
         cin >> sexo;
-        cout << "Indica tu edad";
+        cout << "Indica tu edad ";
         cin >> edad;
-        cout << "Indica tu estatura en m²";
+        cout << "Indica tu estatura en m² ";
         cin >> estatura;
         cout << "Indica tu peso";
         cin >> peso;
-        cout << "Tu IMC es" << peso / pow(estatura,2); 
+        IMC = peso / pow(estatura, 2);
+        cout << "Indica tu peso ";
+        cout << "Tu IMC es " << IMC;
+        if (IMC <= 18.5) {
+            cout << "Usted se encuentra en rango de peso insuficiente";
+        }
+        if (IMC >= 18.5 && IMC < 25) {
+            cout << "Usted se encuentra en rango de peso saludable";
+        }
+        if (IMC >= 25 && IMC < 30) {
+            cout << "Usted tiene sobre peso";
+        }
+        if (IMC >= 30) {
+            cout << "Usted se encuentra dentro del rango de obesidad";
+        }
+        else {
+            cita = "no";
+            cout << "Ten un buen dia ";
+        }
+
+
+
 
     }
-    else { 
-        cita = "no"; 
-        cout << "Ten un buen dia";
-    }
-
-
-
-    
 }
 
