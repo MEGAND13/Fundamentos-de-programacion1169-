6beeca6386152f4e2ae4d85773e9ad8d30c4c882
MEGAND13 <156978608+MEGAND13@users.noreply.github.com>
2024-06-15 22:51:03 -0600

    Add files via upload

---
+++ Adivina quien/011_Adivina_quien.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+int main() {
+    int seleccion;
+    do{
+
+        std::cout << "¡Bienvenido al juego de adivinanzas de videojuegos!\n";
+        std::cout << "Piensa en un personaje de videojuego y responde las siguientes preguntas:\n\n";
+
+        std::string respuesta;
+
+        std::cout << "¿El personaje es humano? (s/n): ";
+        std::cin >> respuesta;
+        if (respuesta == "s") {
+            std::cout << "¿El personaje es de la saga de Mario Bros? (s/n): ";
+            std::cin >> respuesta;
+            if (respuesta == "s") {
+                std::cout << "¿El personaje es Mario? (s/n): ";
+                std::cin >> respuesta;
+                if (respuesta == "s") {
+                    std::cout << "¡Correcto! El personaje que pensaste es Mario.\n";
+                } else {
+                    std::cout << "Lo siento, no pude adivinar el personaje que pensaste.\n";
+                }
+            } else {
+                std::cout << "¿El personaje es de la saga de Zelda? (s/n): ";
+                std::cin >> respuesta;
+                if (respuesta == "s") {
+                    std::cout << "¿El personaje es Link? (s/n): ";
+                    std::cin >> respuesta;
+                    if (respuesta == "s") {
+                        std::cout << "¡Correcto! El personaje que pensaste es Link.\n";
+                    } else {
+                        std::cout << "Lo siento, no pude adivinar el personaje que pensaste.\n";
+                    }
+                } else {
+                    std::cout << "Lo siento, no pude adivinar el personaje que pensaste.\n";
+                }
+            }
+        } else {
+            std::cout << "¿El personaje es un robot? (s/n): ";
+            std::cin >> respuesta;
+            if (respuesta == "s") {
+                std::cout << "¿El personaje es Mega Man? (s/n): ";
+                std::cin >> respuesta;
+                if (respuesta == "s") {
+                    std::cout << "¡Correcto! El personaje que pensaste es Mega Man.\n";
+                } else {
+                    std::cout << "Lo siento, no pude adivinar el personaje que pensaste.\n";
+                }
+            } else {
+                std::cout << "Lo siento, no pude adivinar el personaje que pensaste.\n";
+            }
+        }
+        std::cout<< "Desea volver a jugar? 1)SI, 2)NO \n";
+        std::cin>> seleccion;
+    }while (seleccion == 1);
+}
